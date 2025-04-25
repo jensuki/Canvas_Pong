f326ab0b40ddad25c0f889f737dd7c314824dda7
jensuki <jeni.suk@proton.me>
2025-04-25 14:49:16 -0700

    draw center net onto canvas for testing purposes

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,10 @@
 // ========= draw canvas elements ==========
 
+function drawRect(x, y, w, h, color) {
+    ctx.fillstyle = color;
+    ctx.fillRect(x, y, w, h);
+}
+
 function drawBall(x, y, r, color) {
     ctx.fillStyle = color;
     ctx.beginPath();
@@ -7,4 +12,13 @@ function drawBall(x, y, r, color) {
     ctx.fill();
 }
 
+function drawAll() {
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
+
+    // draw center net
+
+}
 drawBall(ball.x, ball.y, ballRadius, 'white');
+for (let i = 0; i < canvas.height; i += 20.5) {
+    drawRect(canvas.width / 2 - 1, i, 2, 10, 'white');
+}
